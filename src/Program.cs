fc2780a15af74a0ca3c0c04cd2febdc1a9760c12
Aboo <a.azarnoush@pap.dev>
2023-10-15 19:13:48 +1100

    moved settgins

--- src/Program.cs
+++ src/Program.cs
@@ -21,10 +21,10 @@ class Program
     var settings = config.GetRequiredSection(CONFIG_SETTINGS_SECTION_KEY).Get<Settings>() ?? throw new Exception(ERROR_SETTINGS_NOT_DEFINED);
 
     var builder = Host.CreateApplicationBuilder();
+    builder.Services.AddSingleton(settings);
     builder.Services.AddTransient<PingPonger>();
     builder.Services.AddTransient<PullRequestOrganizer>();
     builder.Services.AddHostedService<Bot>();
-    builder.Services.AddSingleton(settings);
 
     var host = builder.Build();
     host.Run();
